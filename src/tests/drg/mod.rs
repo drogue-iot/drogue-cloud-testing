f62a08a78550a8d25a864512e16e522bd798ef98
Jens Reimann <jreimann@redhat.com>
2021-10-14 12:50:56 +0200

    fix: Fix drg version check

--- src/tests/drg/mod.rs
+++ src/tests/drg/mod.rs
@@ -7,7 +7,7 @@ async fn test_drg_version(ctx: &mut TestContext) -> anyhow::Result<()> {
     let drg = ctx.drg().await?;
 
     assert_eq!(
-        r#"Drg Version: 0.5.1
+        r#"Drg Version: 0.7.0
 Connected drogue-cloud service: v0.7.0
 "#,
         drg.version_str().unwrap()
