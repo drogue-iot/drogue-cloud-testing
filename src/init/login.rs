b9d08ba13a71cedc42195261d8c56ba1a3afeec8
Jens Reimann <jreimann@redhat.com>
2021-11-02 14:07:33 +0100

    fix: improve login detection
    * Search in parallel for either the login state, or the login button
    * Search for a button inside the login form

--- src/init/login.rs
+++ src/init/login.rs
@@ -2,38 +2,56 @@ use crate::init::config::Config;
 use crate::init::web::WebDriver;
 use fantoccini::error::CmdError;
 use fantoccini::Locator;
-use std::time::Duration;
-use url::Url;
+use std::time::{Duration, Instant};
 
 /// ensure that the web driver is logged in to the console
 pub async fn login(web: &mut WebDriver, config: &Config) -> anyhow::Result<()> {
     let url = config.console().await?;
 
-    // go to the login page
+    // go to the main page
+
     web.goto(&url).await?;
 
-    match web
-        .wait()
-        .at_most(Duration::from_secs(2))
-        .for_element(Locator::Id("user-dropdown"))
-        .await
-    {
-        Ok(_) => {
-            log::info!("Early return, we are already logged in");
-            return Ok(());
-        }
-        Err(CmdError::WaitTimeout) | Err(CmdError::NoSuchElement(_)) => {
-            log::info!("Performing console login");
-            // continue
-        }
-        Err(err) => return Err(err.into()),
-    }
+    // find either the user dropdown (logged in) or the login button (not logged in)
 
-    web.wait()
-        .for_element(Locator::Css("button.pf-c-button.pf-m-primary"))
-        .await?
-        .click()
-        .await?;
+    let end = Instant::now() + Duration::from_secs(30);
+    let login_button = loop {
+        // find user dropdown
+
+        match web.find(Locator::Id("user-dropdown")).await {
+            Ok(_) => {
+                // we are already logged in, return
+                return Ok(());
+            }
+            Err(CmdError::NoSuchElement(_)) => {}
+            Err(err) => return Err(err.into()),
+        }
+
+        // check login button
+
+        match web
+            .find(Locator::Css(".pf-c-login button.pf-c-button.pf-m-primary"))
+            .await
+        {
+            Ok(login_button) => {
+                break login_button;
+            }
+            Err(CmdError::NoSuchElement(_)) => {}
+            Err(err) => return Err(err.into()),
+        }
+
+        // timeout?
+
+        if end > Instant::now() {
+            anyhow::bail!("Found neither user dropdown nor login button");
+        }
+
+        // wait a bit longer
+
+        tokio::time::sleep(Duration::from_millis(250)).await;
+    };
+
+    login_button.click().await?;
 
     let mut form = web.form(Locator::Id("kc-form-login")).await?;
     form.set_by_name("username", &config.user)
