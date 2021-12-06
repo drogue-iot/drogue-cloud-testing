b47329cfbb81d22b57829e166cea79f6c98ab503
jbtrystram <jbtrystram@redhat.com>
2021-12-06 12:06:27 +0100

    drg version update

--- src/tests/drg/mod.rs
+++ src/tests/drg/mod.rs
@@ -7,7 +7,7 @@ async fn test_drg_version(ctx: &mut TestContext) -> anyhow::Result<()> {
     let drg = ctx.drg().await?;
 
     assert_eq!(
-        r#"Drg Version: 0.7.0
+        r#"Drg Version: 0.8.0
 Connected drogue-cloud service: v0.8.0
 "#,
         drg.version_str().unwrap()
