a2439f69d3562e338561d57f0897327a720e97ff
Jens Reimann <jreimann@redhat.com>
2021-12-14 08:57:19 +0100

    test: update version check

--- src/tests/drg/mod.rs
+++ src/tests/drg/mod.rs
@@ -8,7 +8,7 @@ async fn test_drg_version(ctx: &mut TestContext) -> anyhow::Result<()> {
 
     assert_eq!(
         r#"Drg Version: 0.8.0
-Connected drogue-cloud service: v0.8.0
+Connected drogue-cloud service: v0.9.0
 "#,
         drg.version_str().unwrap()
     );
