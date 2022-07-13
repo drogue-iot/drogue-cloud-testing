b1f73902ab33a5049228e26f432da68457688f04
jbtrystram <jbtrystram@redhat.com>
2022-07-13 14:34:56 +0200

    update drg to latest version

--- src/tests/drg/mod.rs
+++ src/tests/drg/mod.rs
@@ -6,11 +6,9 @@ use test_context::test_context;
 async fn test_drg_version(ctx: &mut TestContext) -> anyhow::Result<()> {
     let drg = ctx.drg().await?;
 
-    assert_eq!(
-        r#"Drg Version: 0.8.0
-Connected drogue-cloud service: v0.10.0
-"#,
-        drg.version_str().unwrap()
+    let version = drg.version()?;
+    assert_eq!("0.10.2",
+        version.get("drg").unwrap().as_str().unwrap()
     );
 
     Ok(())
